c1e5dddfcb536f62a823b6fe66c339e1c6ce2971
stryke <stryke@uw.edu>
2013-02-09 22:46:35 +0000

    Attempted to move files into the default eclipse src folder. Make starter repositories and unit tests.

---
+++ src/domain/Person.java
@@ -0,0 +1,20 @@
+package domain;
+public class Person {
+	
+	private String _firstName;
+	public String getFirstName() {
+		return _firstName;
+	}
+	public void setName(String firstName) {
+		_firstName = firstName;
+	}
+	
+	private String _eMail;
+	public String getEMail() {
+		return _eMail;
+	}
+	public void setEMail(String eMail) {
+		_eMail = eMail;
+	}
+	
+}
