88a86cf3a0510ae04445316acbd68358680c32dc
jamarq@uw.edu <jamarq@uw.edu@e8a57f98-f285-ff2f-9b05-89c155bca2d0>
2013-02-15 19:36:23 +0000

    Login button action added.

--- trunk/src/view/HeaderPanel.java
+++ trunk/src/view/HeaderPanel.java
@@ -3,12 +3,16 @@ package view;
 import java.awt.Color;
 import java.awt.Component;
 import java.awt.Dimension;
+import java.awt.event.ActionEvent;
 
+import javax.swing.AbstractAction;
 import javax.swing.JButton;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JPasswordField;
 import javax.swing.JTextField;
+
+import model.Person;
 
 import controller.Controls;
 
@@ -41,6 +45,11 @@ public class HeaderPanel extends JPanel {
 	 */
 	JLabel my_password_label;
 	
+	/**
+	 * Creates a header panel when not yet logged in.
+	 * 
+	 * @param the_controller That created this header.
+	 */
 	public HeaderPanel(final Controls the_controller) {
 		
 		setBackground(Color.BLUE);
@@ -59,7 +68,19 @@ public class HeaderPanel extends JPanel {
 		add(my_username_input);
 		my_username_input.setColumns(10);
 		
-		my_submit_button = new JButton("Submit");
+		
+		my_submit_button = new JButton(new AbstractAction("Submit") {
+			
+			public void actionPerformed(ActionEvent e) {
+				
+				//Sends username and password to controller login on button press.
+				the_controller.login(my_username_input.getText(), 
+						my_password_field.getPassword());
+				
+			}
+			
+		});
+		
 		my_submit_button.setBounds(308, 73, 89, 23);
 		add(my_submit_button);
 		
@@ -70,6 +91,21 @@ public class HeaderPanel extends JPanel {
 		my_password_label = new JLabel("Password");
 		my_password_label.setBounds(223, 42, 46, 20);
 		add(my_password_label);
+		
+	}
+	
+	/**
+	 * Creates a header panel when a user has logged in.
+	 * 
+	 * @param the_controller The controller that created this header.
+	 * @param the_person The person that is logged in.
+	 */
+	public HeaderPanel(Controls the_controller, Person the_person) {
+		
+		my_username_label = new JLabel("Welcome " + the_person.getFirstName() +
+				the_person.getLastName());
+		my_username_label.setBounds(221, 14, 48, 14);
+		add(my_username_label);
 		
 	}
 
