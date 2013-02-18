fb02e44b187a65a75754102348e6e0d916c98819
jamarq@uw.edu <jamarq@uw.edu@e8a57f98-f285-ff2f-9b05-89c155bca2d0>
2013-02-18 06:45:33 +0000

    login method now creates new person and updates header.

--- 360 Weaving Matt/src/controller/Controls.java
+++ 360 Weaving Matt/src/controller/Controls.java
@@ -3,60 +3,90 @@
  */
 package controller;
 
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
+import repository.CategoryRepository;
+import repository.EntryRepository;
+import repository.PersonRepository;
+import view.EntrantHomeBody;
+import view.InputEntryBody;
+import view.RegisterBody;
+import view.ViewEntryBody;
 import view.WeaveGUI;
 import model.Person;
-import model.PersonInfo;
+import model.Entry;
 
 /**
  *
  */
 public class Controls implements ControlInterface {
-    Person  my_person;
-    WeaveGUI my_view;
+    Person  _person;
+    WeaveGUI _view;
+    PersonRepository _personRepo;
+    EntryRepository _entryRepo;
+    CategoryRepository _categoryRepo;
     
     
-    public Controls() {
-       my_person = new PersonInfo();
-       my_view = new WeaveGUI(this, my_person);
+    public Controls() throws FileNotFoundException, IOException {
+       _person = new Person();
+       _view = new WeaveGUI(this);
+       _personRepo = new PersonRepository();
+       _entryRepo = new EntryRepository();
+       _categoryRepo = new CategoryRepository();
     }
     /**
      * {@inheritDoc}
      */
     @Override
     public void start() {
-        my_view.createView();
+        _view.createView();
     }
+    
     /**
      * {@inheritDoc}
      */
     @Override
-    public void login() {
-        // TODO Auto-generated method stub
-
+    public void login(String the_username, String the_password) {
+    		try {
+				_person = _personRepo.getByLogin(the_username, the_password);
+			} catch (ClassNotFoundException e) {
+				System.out.println("Class not found");
+			} catch (IOException e) {
+				System.out.println("Invalid file");
+			}
+    		
+    		_view.setBody(new RegisterBody(this));
+    		_view.setHeader(this, _person);
     }
     /**
      * {@inheritDoc}
      */
     @Override
     public void register() {
-        // TODO Auto-generated method stub
+        _view.setBody(new EntrantHomeBody(this));
 
     }
     /**
      * {@inheritDoc}
      */
     @Override
-    public void inputEntry() {
-        // TODO Auto-generated method stub
-
+    public void inputEntry(Entry entry) {
+    	_view.setBody(new InputEntryBody(this, entry));
     }
     /**
      * {@inheritDoc}
+     * @throws IOException 
      */
     @Override
-    public void submitEntry() {
-        // TODO Auto-generated method stub
-
+    public void submitEntry(Entry entry) throws IOException {
+    	_entryRepo.add(entry);
+    	_view.setBody(new ViewEntryBody(this, entry));
+    }
+    
+    public void editEntry(Entry entry) throws IOException {
+    	_entryRepo.update(entry);
+    	_view.setBody(new ViewEntryBody(this, entry));
     }
 
     /**
