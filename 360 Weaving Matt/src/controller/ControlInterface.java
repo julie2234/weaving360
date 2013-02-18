e4ded6c851771ed89b7cdb0e1acb493e50177f52
jamarq@uw.edu <jamarq@uw.edu@e8a57f98-f285-ff2f-9b05-89c155bca2d0>
2013-02-18 06:44:43 +0000

    login() paramaters changed to username and password

--- 360 Weaving Matt/src/controller/ControlInterface.java
+++ 360 Weaving Matt/src/controller/ControlInterface.java
@@ -2,16 +2,26 @@
  * 
  */
 package controller;
+import java.io.IOException;
+
+import model.Entry;
 
 /**
  *
  */
 public interface ControlInterface {
     void start();
-    void login();
+    /**
+     * Starts view rebuild based on user.
+     * 
+     * @param the_username The username provided in header panel.
+     * @param the_password The passowrd provided in header panel.
+     */
+    void login(String the_username, String the_password);
     void register();
-    void inputEntry();
-    void submitEntry();
+    void inputEntry(Entry entry);
+    void submitEntry(Entry entry) throws IOException;
+    void editEntry(Entry entry) throws IOException;
     void editAccountInfo();
     /*
      * viewContestants, viewCategories, etc.    
