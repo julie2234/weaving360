ebd206db0ae002dfc967f4809fc3735ce46b1800
stryke <stryke@uw.edu>
2013-02-19 03:25:30 +0000

    Added Javadocs for repository objects.

--- src/repository/AbstractRepository.java
+++ src/repository/AbstractRepository.java
@@ -17,24 +17,70 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Properties;
 
-import model.Person;
-
+/**
+ * An abstract repository that can load, save, delete, and get all objects to
+ * and from the file system.
+ * 
+ * @author Ralph Feltis
+ * @version 20130218
+ * @param <T>
+ *            The generic type that will be used by this repository.
+ */
 public abstract class AbstractRepository<T> {
 
 	private final static String DEFAULT_CONFIG_FILENAME = "weaving.cfg";
 
 	private String _dataFolder;
 
+	/**
+	 * Constructs an abstract repository.
+	 * 
+	 * @param configFileName
+	 *            The filename of the configuration file.
+	 * @param propertySetting
+	 *            The key of the key/value pair in the configuration file that
+	 *            corresponds to the repository.
+	 * @throws FileNotFoundException
+	 *             The exception thrown if the configuration file cannot be
+	 *             found.
+	 * @throws IOException
+	 *             The IO exception thrown if there is an IO error while reading
+	 *             the configuration file.
+	 */
 	public AbstractRepository(String configFileName, String propertySetting)
 			throws FileNotFoundException, IOException {
 		setDataFolder(configFileName, propertySetting);
 	}
 
+	/**
+	 * Constructs an abstract repository using the default configuration file
+	 * name of weaving.cfg.
+	 * 
+	 * @param propertySetting
+	 *            The key of the key/value pair in the configuration file that
+	 *            corresponds to the repository.
+	 * @throws FileNotFoundException
+	 *             The exception thrown if the configuration file cannot be
+	 *             found.
+	 * @throws IOException
+	 *             The IO exception thrown if there is an IO error while reading
+	 *             the configuration file.
+	 */
 	public AbstractRepository(String propertySetting)
 			throws FileNotFoundException, IOException {
 		setDataFolder(DEFAULT_CONFIG_FILENAME, propertySetting);
 	}
 
+	/**
+	 * Get every object found in a folder.
+	 * 
+	 * @return A list of objects.
+	 * @throws ClassNotFoundException
+	 *             The exception thrown if the generic class cannot be found.
+	 * @throws IOException
+	 *             The IO exception thrown if there is an IO error while reading
+	 *             the object file.
+	 */
 	public List<T> getAll() throws ClassNotFoundException, IOException {
 		List<T> items = new ArrayList<T>();
 		for (String fileName : getDataFolder().list()) {
@@ -46,6 +92,17 @@ public abstract class AbstractRepository<T> {
 		return items;
 	}
 
+	/**
+	 * Saves an object to the file system using serialization.
+	 * 
+	 * @param entity
+	 *            The object to save.
+	 * @param fileName
+	 *            The filename for the object.
+	 * @throws IOException
+	 *             The IO exception thrown if there is an IO error while saving
+	 *             the object file.
+	 */
 	protected void saveObject(T entity, String fileName) throws IOException {
 		File folder = new File(_dataFolder);
 		if (!folder.exists()) {
@@ -59,11 +116,23 @@ public abstract class AbstractRepository<T> {
 		output.close();
 	}
 
+	/**
+	 * Loads an object from the file system based on serialization.
+	 * 
+	 * @param fileName
+	 *            The filename of the object.
+	 * @return The instantiated object.
+	 * @throws IOException
+	 *             The IO exception thrown if there is an IO error while reading
+	 *             the object file.
+	 * @throws ClassNotFoundException
+	 *             The exception thrown if the generic class cannot be found.
+	 */
 	@SuppressWarnings("unchecked")
 	protected T loadObject(String fileName) throws IOException,
 			ClassNotFoundException {
 		File file = getDataFile(fileName);
-		
+
 		T loadedObject = null;
 		if (file.exists() && !file.isDirectory()) {
 			InputStream inputStream = new FileInputStream(file);
@@ -78,6 +147,12 @@ public abstract class AbstractRepository<T> {
 		return loadedObject;
 	}
 
+	/**
+	 * Deletes a file in the file system.
+	 * 
+	 * @param fileName
+	 *            The filename of the file to delete.
+	 */
 	protected void deleteFile(String fileName) {
 		File file = getDataFile(fileName);
 		if (file.exists()) {
@@ -85,20 +160,49 @@ public abstract class AbstractRepository<T> {
 		}
 	}
 
+	/**
+	 * Sets the data folder for this repository.
+	 * 
+	 * @param configFileName
+	 *            The filename of the configuration file.
+	 * @param propertySetting
+	 *            The key of the key/value pair in the configuration file that
+	 *            corresponds to the repository.
+	 * @throws FileNotFoundException
+	 *             The exception thrown if the configuration file cannot be
+	 *             found.
+	 * @throws IOException
+	 *             The IO exception thrown if there is an IO error while reading
+	 *             the configuration file.
+	 */
 	private final void setDataFolder(String configFileName,
 			String propertySetting) throws FileNotFoundException, IOException {
 		Properties settings = new Properties();
 		settings.load(new FileInputStream(System.getProperty("user.dir")
 				+ File.separator + configFileName));
-		_dataFolder = System.getProperty("user.dir") + File.separator
-				+ settings.getProperty(propertySetting).replace("//",  File.separator)
-				.replace("\\", File.separator);
+		_dataFolder = System.getProperty("user.dir")
+				+ File.separator
+				+ settings.getProperty(propertySetting)
+						.replace("//", File.separator)
+						.replace("\\", File.separator);
 	}
 
+	/**
+	 * Get a File object for this repository's data folder.
+	 * 
+	 * @return The File object for this repository's data folder.
+	 */
 	private File getDataFolder() {
 		return new File(_dataFolder);
 	}
 
+	/**
+	 * Get a File object for a specified file name.
+	 * 
+	 * @param fileName
+	 *            The filename to load.
+	 * @return The File object for the requested filename.
+	 */
 	private File getDataFile(String fileName) {
 		return new File(_dataFolder + File.separator + fileName);
 	}
--- src/repository/CategoryRepository.java
+++ src/repository/CategoryRepository.java
@@ -14,29 +14,82 @@ public class CategoryRepository extends AbstractRepository<Category> {
 
 	private PersonRepository _personRepository;
 
+	/**
+	 * Constructs a category repository with the default configuration file name
+	 * of weaving.cfg for adding, removing, updating, and retrieving category
+	 * objects.
+	 * 
+	 * @throws FileNotFoundException
+	 *             The exception thrown if the default configuration file is not
+	 *             found.
+	 * @throws IOException
+	 *             The IO exception thrown if there is an IO error while reading
+	 *             the configuration file.
+	 */
 	public CategoryRepository() throws FileNotFoundException, IOException {
 		super(PROPERTY_NAME);
 		_personRepository = new PersonRepository();
 	}
 
+	/**
+	 * Constructs a category repository for adding, removing, updating, and
+	 * retrieving category objects.
+	 * 
+	 * @throws FileNotFoundException
+	 *             The exception thrown if the default configuration file is not
+	 *             found.
+	 * @throws IOException
+	 *             The IO exception thrown if there is an IO error while reading
+	 *             the configuration file.
+	 */
 	public CategoryRepository(String configFileName)
 			throws FileNotFoundException, IOException {
 		super(configFileName, PROPERTY_NAME);
-		_personRepository = new PersonRepository();
+		_personRepository = new PersonRepository(configFileName);
 	}
 
+	/**
+	 * Adds a category to the repository.
+	 * 
+	 * @param category
+	 *            The Category object to add to the repository.
+	 * @throws IOException
+	 *             The IO exception thrown if there is an IO error while saving
+	 *             the object.
+	 */
 	public void add(Category category) throws IOException {
 		saveObject(category, category.getName());
 	}
 
+	/**
+	 * Updates a category to the repository.
+	 * 
+	 * @param category
+	 *            The category object to update to the repository.
+	 * @throws IOException
+	 *             The IO exception thrown if there is an IO error while
+	 *             updating the object.
+	 */
 	public void update(Category category) throws IOException {
 		saveObject(category, category.getName());
 	}
 
+	/**
+	 * Removes a category from the repository.
+	 * 
+	 * @param category
+	 *            The category object to remove from the repository.
+	 * @throws IOException
+	 *             The IO exception thrown if there is an IO error while
+	 *             removing the object.
+	 */
 	public void remove(Category category) throws IOException {
 		deleteFile(category.getName());
 	}
 
+	/**
+	 * Retrieves all categories.
+	 */
 	@Override
 	public List<Category> getAll() throws ClassNotFoundException, IOException {
 		List<Category> allCategories = super.getAll();
@@ -46,6 +99,18 @@ public class CategoryRepository extends AbstractRepository<Category> {
 		return allCategories;
 	}
 
+	/**
+	 * Get a category based on its name.
+	 * 
+	 * @param name
+	 *            The name of the category
+	 * @return The Category object that was loaded from the repository.
+	 * @throws IOException
+	 *             The IO exception thrown if there is an IO error while
+	 *             retrieving the object.
+	 * @throws ClassNotFoundException
+	 *             The exception thrown if the Category class cannot be found.
+	 */
 	public Category getByName(String name) throws IOException,
 			ClassNotFoundException {
 		Category category = loadObject(name);
@@ -53,8 +118,21 @@ public class CategoryRepository extends AbstractRepository<Category> {
 		return category;
 	}
 
-	private void reloadCachedJudges(Category category)
-			throws FileNotFoundException, IOException, ClassNotFoundException {
+	/**
+	 * Reloads the person objects. The person objects that are loaded will be
+	 * the person objects as they were when this object was last saved to the
+	 * repository.
+	 * 
+	 * @param category
+	 *            The Category object that needs its people reloaded.
+	 * @throws IOException
+	 *             The IO exception thrown if there is an IO error while
+	 *             reloading a person.
+	 * @throws ClassNotFoundException
+	 *             The exception thrown if the Person class cannot be found.
+	 */
+	private void reloadCachedJudges(Category category) throws IOException,
+			ClassNotFoundException {
 		if (category != null) {
 			List<Person> freshJudges = new ArrayList<Person>();
 			for (Person person : category.getJudges()) {
