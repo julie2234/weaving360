446ee793e45efadaae73abe5711c9f421623f34e
julieimpola <julieimpola@gmail.com>
2013-03-08 02:37:05 +0000

    Finished Organizer panels.

--- src/view/OrganizerListPeoplePanel.java
+++ src/view/OrganizerListPeoplePanel.java
@@ -7,8 +7,11 @@ import javax.swing.BorderFactory;
 import javax.swing.BoxLayout;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.JScrollPane;
 import javax.swing.JTable;
 import javax.swing.table.DefaultTableModel;
+import javax.swing.table.TableModel;
+import javax.swing.table.TableRowSorter;
 
 import model.Person;
 import model.Role;
@@ -33,22 +36,23 @@ public class OrganizerListPeoplePanel extends JPanel {
 		this.setBorder(BorderFactory.createEmptyBorder(0, 20, 0, 20));
 
 		if(people.size() > 0) {
-			String[] column = {"Last Name", "First Name", "Email", "Phone Number"};
-			Object[][] data = new Object[people.size()][column.length];
+			String[] columns = {"Last Name", "First Name", "Email", "Phone Number", "Role"};
+			Object[][] data = new Object[people.size()][columns.length];
 			for(int i = 0; i < people.size(); i++) {
 				data[i][0] = people.get(i).getLastName();
 				data[i][1] = people.get(i).getFirstName();
 				data[i][2] = people.get(i).getEMail();
 				data[i][3] = people.get(i).getPhoneNumber();
+				data[i][4] = people.get(i).getRole();
 			}
-			DefaultTableModel model = new DefaultTableModel(data, column);
-	        _table = new JTable(model);
+			DefaultTableModel model = new DefaultTableModel(data, columns);
+			_table = new JTable(model);
+			_table.setAutoCreateRowSorter(true);
 			this.add(new JLabel("All people currently registered as " + role + "s."));
 			_table.setEnabled(false);
-			_table.setAutoResizeMode(5);
-			_table.getTableHeader().setEnabled(false);
-			this.add(_table.getTableHeader());
-			this.add(_table);
+			_table.setAutoResizeMode(4);
+			JScrollPane pane = new JScrollPane(_table);
+			this.add(pane);
 		}
 		else if(people.size() == 0) {  //No people in that role.
 			this.add(new JLabel("There are no registered " + role + "s yet."));
