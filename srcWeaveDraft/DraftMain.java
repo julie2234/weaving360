e3945b64b165c913b8dbe09f0d0797ea71c4d767
mbadams29 <mbadams29@gmail.com>
2013-03-09 06:39:22 +0000

    Latest version of WeaveDraft. You can adjust size of tie-up and grid size to a max of 36 grid size, and 8 tie-up size.

--- srcWeaveDraft/DraftMain.java
+++ srcWeaveDraft/DraftMain.java
@@ -1,21 +1,27 @@
-import javax.swing.JFrame;
+import java.awt.Dimension;
 
+import javax.swing.JFrame;
 
 public class DraftMain {
 
   /**
-   * @param args
+   * Main to test WeaveDraft.
+   * @param args Main arguments.
    */
   public static void main(String[] args) {
-    JFrame my_frame = new JFrame();
-    WeaveDraft weave = new WeaveDraft(16, 4);
-    my_frame.add(weave);
-    my_frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-    //my_frame.setPreferredSize(FRAME_DIM);
-    my_frame.pack();
-    my_frame.setLocationRelativeTo(null);
-    my_frame.setVisible(true);
-
+      //Change to test resizing
+      int gridSize = 24;
+      int tieUp = 4;
+      
+      JFrame my_frame = new JFrame();
+      WeaveDraft weave = new WeaveDraft(gridSize, tieUp);
+      my_frame.add(weave);
+      //my_frame.setMinimumSize(new Dimension((int) (gridSize*37.5), tieUp * 150));
+      my_frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+      //my_frame.setPreferredSize(FRAME_DIM);
+      my_frame.pack();
+      my_frame.setMinimumSize(my_frame.getSize());
+      my_frame.setLocationRelativeTo(null);
+      my_frame.setVisible(true);
   }
-
 }
