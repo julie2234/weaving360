1844c620b443fd3e3d3d6e8cf500617c54333219
julieimpola@gmail.com <julieimpola@gmail.com@e8a57f98-f285-ff2f-9b05-89c155bca2d0>
2013-03-11 17:03:56 +0000

    Changed "Just BeWeave's" to "JBW's" so the title doesn't get cut off on DefaultBody.

--- trunk/src/view/EntrantHomeBody.java
+++ trunk/src/view/EntrantHomeBody.java
@@ -1,19 +1,13 @@
 package view;
 
-import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.Dimension;
-import java.awt.GridBagConstraints;
-import java.awt.GridBagLayout;
 import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
-import java.awt.event.WindowAdapter;
-import java.awt.event.WindowEvent;
-import java.awt.event.WindowListener;
 import java.awt.font.TextAttribute;
 import java.awt.image.BufferedImage;
 
@@ -22,12 +16,9 @@ import javax.swing.BorderFactory;
 import javax.swing.BoxLayout;
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
-import javax.swing.JDialog;
 import javax.swing.JLabel;
 import javax.swing.JOptionPane;
 import javax.swing.JPanel;
-import javax.swing.SwingConstants;
-
 import controller.Controls;
 
 import java.awt.Font;
--- trunk/src/view/InputEntryBody.java
+++ trunk/src/view/InputEntryBody.java
@@ -3,7 +3,6 @@ package view;
 
 import java.awt.Dimension;
 import java.awt.Font;
-import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.io.IOException;
